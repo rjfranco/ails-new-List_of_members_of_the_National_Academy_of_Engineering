e06353d3611c4a4bf2d174820356465e23d5f0ac
sokampdx <sokam@pdx.edu>
2016-06-07 19:31:13 -0700

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_List_of_members_of_the_National_Academy_of_Engineering_session'
